c5424fbf06eadb53afe694275c043710496ecea3
EDA <eda@mac-mini-1.local>
2017-05-04 17:32:16 +1200

    initial files

---
+++ routes.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const backUp = require('./backup.js')
+const router = express.Router()
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+
+const routes = require('./routes')
+
+const app = express()
+
+const hbsConfig = {
+  extname: 'hbs',
+  defaultLayout: 'main'
+}
+
+app.engine('hbs', hbs(hbsConfig))
+app.set('view engine', 'hbs')
+app.use('/', routes)
+
+module.exports = app
